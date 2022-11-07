1455d1172463c36191d5dc84238861a8fcf354f5
Zeeshan Ali <zeeshanalik73@gmail.com>
2022-11-07 19:05:45 +0500

    Fixes

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -35,12 +35,12 @@ android {
 
 dependencies {
     implementation(project(":shared"))
-    implementation("androidx.compose.ui:ui:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling:1.2.1")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.2.1")
-    implementation("androidx.compose.foundation:foundation:1.2.1")
-    implementation("androidx.compose.material:material:1.2.1")
-    implementation("androidx.activity:activity-compose:1.6.0")
+    implementation("androidx.compose.ui:ui:1.3.0")
+    implementation("androidx.compose.ui:ui-tooling:1.3.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.3.0")
+    implementation("androidx.compose.foundation:foundation:1.3.0")
+    implementation("androidx.compose.material:material:1.3.0")
+    implementation("androidx.activity:activity-compose:1.6.1")
 
 //    Dagger Hilt for DI
     implementation("com.google.dagger:hilt-android:2.44")
